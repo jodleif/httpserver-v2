1a4c15583aab928784fef97be7f8cdef810a186f
Jo Øivind Gjernes <jodleif@gmail.com>
2018-03-01 23:58:00 +0100

    Remove dead code in binary_file.cpp

--- src/binary_file.cpp
+++ src/binary_file.cpp
@@ -1,15 +1,8 @@
 #include "binary_file.h"
 #include "hash_literal.h"
 // Expected files for one-page supplescroll
-/*extern "C" const char _binary_dark_css_start;
-extern "C" const char _binary_dark_css_end;*/
 extern "C" const char _binary_index_html_gz_start;
 extern "C" const char _binary_index_html_gz_end;
-/*
-extern "C" const char _binary_supplescroll_css_start;
-extern "C" const char _binary_supplescroll_css_end;
-extern "C" const char _binary_supplescroll_min_js_start;
-extern "C" const char _binary_supplescroll_min_js_end;*/
 constexpr char empty_file[] = "";
 
 namespace blob_files {
@@ -28,16 +21,9 @@ get_file(std::string path)
 {
     switch (util::fnv1a(path.data(), path.length())) {
         case "/"_f:
+        case "/index.htm"_f:
         case "/index.html"_f:
             return fetch_blob(&_binary_index_html_gz_start, &_binary_index_html_gz_end);
-        case "/supplescroll.css"_f:
-            /*return fetch_blob(&_binary_supplescroll_css_start,
-                              &_binary_supplescroll_css_end);*/
-        case "/dark.css"_f:
-            /*return fetch_blob(&_binary_dark_css_start, &_binary_dark_css_end);*/
-        case "/supplescroll.min.js"_f:
-            /*return fetch_blob(&_binary_supplescroll_min_js_start,
-                              &_binary_supplescroll_min_js_end);*/
         default:
             return empty_file;
     }
